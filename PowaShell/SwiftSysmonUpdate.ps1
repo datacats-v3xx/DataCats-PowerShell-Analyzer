1ebc34e8654366137d943b4dff9f9aa46c462961
datacats-v3xx <randy@obriant.me>
2025-02-28 22:43:34 -0600

    Delete PowaShell/SwiftSysmonUpdate.ps1

--- PowaShell/SwiftSysmonUpdate.ps1
+++
@@ -1,2 +0,0 @@
-﻿Invoke-WebRequest -Uri "https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml" -OutFile "$env:TEMP\sysmonconfig-export.xml"
-& "$env:TEMP\Sysmon\Sysmon64.exe" -c "$env:TEMP\sysmonconfig-export.xml"
